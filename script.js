c325209e1f125cac6cb4dd6927235dd8ce1a8146
Davy Nyakinyua <davynyakingua@gmail.com>
2025-07-07 22:43:09 +0300

    Create algorithm of project and break down tasks

--- script.js
+++ script.js
@@ -1,1 +1,89 @@
 console.log("Hello world");
+
+// initialize the an array to store all the books
+
+const library = [];
+
+let bookTitle;
+
+//create a constructor function for books
+
+function Books (name, author, pages) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+}
+
+
+// Add a prototype readUnread() method for read and unread status of the book
+
+Books.prototype.readUnread = function (){
+    // change background color of button to purple
+}
+
+
+// Add a prototype for the method of a unique id crypto.randomUUID()
+
+Books.prototype.id = function () {
+
+    let id = crypto.randomUUID();
+
+    return id;
+}
+
+
+// A function for creating an individual books and add to array library
+
+function book (name, author, pages) {
+
+   bookTitle = new Books(name, author, pages);
+
+    library.push(bookTitle);
+
+    return display();
+
+}
+
+
+// function for the display of books on the viewport
+// Dom manipulation
+
+function display () {
+
+    // create container element
+
+    let container = document.getElementById("content");
+
+    // create a grid dynamically using a loop
+
+    for(let i = 0; i < library.length; i++){
+
+        let contentContainer = document.createElement("div");
+
+        contentContainer.className = "contentContainer"
+
+        contentContainer.innerText = bookTitle.name
+
+        container.appendChild(contentContainer);
+
+    }
+
+}
+
+
+// A button and event listener that calls the addNewBook function
+
+
+
+
+// function removeBook for removing book from library
+// Button that call function removeBook
+
+
+
+
+
+
+// A button that call readUnread() method prototype of constuctor function
+
+
